4d384e3f988f84afc57b3bdd9a39559b565c1b25
Olena-Andrushchenko <olena.anru.anru@gmail.com>
2024-07-06 00:01:13 +0200

    added taskCompletionChart function

--- app.js
+++ app.js
@@ -28,6 +28,37 @@ async function printTable() {
             `;
             tableBody.appendChild(row);
         });
+
+        const ctx = document.getElementById('taskCompletionChart').getContext('2d');
+        const doneCount = results.filter(item => item.isDone).length;
+        const notDoneCount = results.length - doneCount;
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Done', 'Not Done'],
+                datasets: [{
+                    label: 'Task Completion Status',
+                    data: [doneCount, notDoneCount],
+                    backgroundColor: [
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 99, 132, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 99, 132, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+            });
     } catch (error) {
         console.error('Error fetching or processing data:', error);
     }
